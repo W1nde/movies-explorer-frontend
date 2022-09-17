d38ab2048d80f94bf4ce296a8f4b1e6255e88084
W1nde <45940099+W1nde@users.noreply.github.com>
2022-09-17 21:24:59 +0700

    Delete src directory

--- src/contexts/infotooltip-context.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export const defaultInfoToolTipState = {
-  isOpen: false,
-  message: "",
-  success: false
-};
-
-export const InfoToolTipContext = createContext();
--- src/contexts/store.js
+++
@@ -1,5 +0,0 @@
-import { MovieContext } from "./movie-context";
-import { CurrentUserContext } from "./user-context";
-import { InfoToolTipContext } from "./infotooltip-context";
-
-export { MovieContext, CurrentUserContext, InfoToolTipContext };
